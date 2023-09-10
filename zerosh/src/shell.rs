eb5216f4771497086fb517a48b97cfe311a01f89
cutsea110 <cutsea110@gmail.com>
2023-09-10 11:08:37 +0900

    start project

---
+++ zerosh/src/shell.rs
@@ -0,0 +1,20 @@
+use crate::helper::DynError;
+use nix::{
+    libc,
+    sys::{
+        signal::{killpg, signal, SigHandler, Signal},
+        wait::{waitpid, WaitPidFlag, WaitStatus},
+    },
+    unistd::{self, dup2, execvp, fork, pipe, setpgid, tcgetpgrp, tcsetpgrp, ForkResult, Pid},
+};
+use rustyline::{error::ReadlineError, Editor};
+use signal_hook::{consts::*, iterator::Signals};
+use std::{
+    collections::{BTreeMap, HashMap, HashSet},
+    ffi::CString,
+    mem::replace,
+    path::PathBuf,
+    process::exit,
+    sync::mpsc::{channel, sync_channel, Receiver, Sender, SyncSender},
+    thread,
+};
