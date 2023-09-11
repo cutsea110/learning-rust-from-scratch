9a2473e537a5fb75215dafed2c36db36c6084789
cutsea110 <cutsea110@gmail.com>
2023-09-11 12:58:05 +0900

    build in commands parser

--- zerosh/src/parser.rs
+++ zerosh/src/parser.rs
@@ -2,6 +2,14 @@ mod combinator;
 
 use combinator::*;
 
+#[derive(Debug)]
+enum BuiltInCmd {
+    Exit(Option<i32>),
+    Jobs,
+    Fg(i32),
+    Cd(String),
+}
+
 fn exit_cmd() -> impl Parser<Output = Option<i32>> {
     skip(literal("exit"), optional(int32()))
 }
@@ -16,4 +24,17 @@ fn fg_cmd() -> impl Parser<Output = i32> {
 
 fn cd_cmd() -> impl Parser<Output = String> {
     skip(literal("cd"), string())
+}
+
+fn build_in_cmd() -> impl Parser<Output = BuiltInCmd> {
+    altl(
+        apply(exit_cmd(), BuiltInCmd::Exit),
+        altl(
+            apply(jobs_cmd(), |_| BuiltInCmd::Jobs),
+            altl(
+                apply(fg_cmd(), BuiltInCmd::Fg),
+                apply(cd_cmd(), BuiltInCmd::Cd),
+            ),
+        ),
+    )
 }
