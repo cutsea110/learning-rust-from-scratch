83e80c59480507bdb4d35f1c00f7fcc4707ceff2
cutsea110 <cutsea110@gmail.com>
2023-09-19 07:20:14 +0900

    Display for ExternalCmd

--- zerosh/src/model.rs
+++ zerosh/src/model.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum BuiltInCmd {
     Exit(Option<i32>),
@@ -26,11 +28,12 @@ pub enum Pipeline {
     Both(Box<Pipeline>, ExternalCmd),
 }
 impl Pipeline {
+    // TODO: Display にする
     pub fn cmd_line(&self) -> String {
         match &self {
-            Pipeline::Src(cmd) => cmd.cmd_line(),
-            Pipeline::Out(p, cmd) => format!("{} | {}", p.cmd_line(), cmd.cmd_line()),
-            Pipeline::Both(p, cmd) => format!("{} |& {}", p.cmd_line(), cmd.cmd_line()),
+            Pipeline::Src(cmd) => format!("{}", cmd),
+            Pipeline::Out(p, cmd) => format!("{} | {}", p.cmd_line(), cmd),
+            Pipeline::Both(p, cmd) => format!("{} |& {}", p.cmd_line(), cmd),
         }
     }
 }
@@ -45,13 +48,18 @@ impl ExternalCmd {
         assert_ne!(self.args.len(), 0);
         &self.args[0]
     }
-
-    pub fn cmd_line(&self) -> String {
-        self.args[0..]
-            .iter()
-            .map(|s| s.as_str())
-            .collect::<Vec<&str>>()
-            .join(" ")
+}
+impl fmt::Display for ExternalCmd {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.args[0..]
+                .iter()
+                .map(|s| s.as_str())
+                .collect::<Vec<&str>>()
+                .join(" ")
+        )
     }
 }
 
