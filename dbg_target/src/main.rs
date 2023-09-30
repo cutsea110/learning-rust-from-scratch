d6f01313f4b265509bb39fbca4c19f2e9efa180f
cutsea110 <cutsea110@gmail.com>
2023-09-30 10:21:20 +0900

    experimental code

--- dbg_target/src/main.rs
+++ dbg_target/src/main.rs
@@ -1,3 +1,19 @@
+use nix::{
+    sys::signal::{kill, Signal},
+    unistd::getpid,
+};
+use std::arch::asm;
+
 fn main() {
-    println!("Hello, world!");
+    println!("int 3");
+    unsafe { asm!("int 3") };
+
+    println!("kill -SIGTRAP");
+    let pid = getpid();
+    kill(pid, Signal::SIGTRAP).unwrap();
+
+    for i in 0..3 {
+        unsafe { asm!("nop") };
+        println!("i = {i}");
+    }
 }
