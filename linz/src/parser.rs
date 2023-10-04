ac24e7b790f4962e10b31878da2ed3f9132dcb16
cutsea110 <cutsea110@gmail.com>
2023-10-05 08:16:22 +0900

    add tokenize tests

--- linz/src/parser.rs
+++ linz/src/parser.rs
@@ -57,3 +57,103 @@ fn tokenize(line: &str) -> Vec<(usize, String)> {
     }
     result
 }
+
+#[cfg(test)]
+mod tokenize {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(tokenize(""), vec![]);
+        assert_eq!(tokenize("foo"), vec![(0, "foo".to_string())]);
+        assert_eq!(tokenize("42"), vec![(0, "42".to_string())]);
+        assert_eq!(
+            tokenize("lin fn x : lin bool {if x {lin false} else {lin true}}"),
+            vec![
+                (0, "lin".to_string()),
+                (4, "fn".to_string()),
+                (7, "x".to_string()),
+                (9, ":".to_string()),
+                (11, "lin".to_string()),
+                (15, "bool".to_string()),
+                (20, "{".to_string()),
+                (21, "if".to_string()),
+                (24, "x".to_string()),
+                (26, "{".to_string()),
+                (27, "lin".to_string()),
+                (31, "false".to_string()),
+                (36, "}".to_string()),
+                (38, "else".to_string()),
+                (43, "{".to_string()),
+                (44, "lin".to_string()),
+                (48, "true".to_string()),
+                (52, "}".to_string()),
+                (53, "}".to_string())
+            ]
+        );
+        assert_eq!(
+            tokenize("un fn x : lin(lin bool * lin bool){split x as a,b {lin <b, a>}}"),
+            vec![
+                (0, "un".to_string()),
+                (3, "fn".to_string()),
+                (6, "x".to_string()),
+                (8, ":".to_string()),
+                (10, "lin".to_string()),
+                (13, "(".to_string()),
+                (14, "lin".to_string()),
+                (18, "bool".to_string()),
+                (23, "*".to_string()),
+                (25, "lin".to_string()),
+                (29, "bool".to_string()),
+                (33, ")".to_string()),
+                (34, "{".to_string()),
+                (35, "split".to_string()),
+                (41, "x".to_string()),
+                (43, "as".to_string()),
+                (46, "a".to_string()),
+                (47, ",".to_string()),
+                (48, "b".to_string()),
+                (50, "{".to_string()),
+                (51, "lin".to_string()),
+                (55, "<".to_string()),
+                (56, "b".to_string()),
+                (57, ",".to_string()),
+                (59, "a".to_string()),
+                (60, ">".to_string()),
+                (61, "}".to_string()),
+                (62, "}".to_string())
+            ]
+        );
+        assert_eq!(
+            tokenize("lin fn x : lin (lin bool * lin bool) {split x as a, b {free b; a}}"),
+            vec![
+                (0, "lin".to_string()),
+                (4, "fn".to_string()),
+                (7, "x".to_string()),
+                (9, ":".to_string()),
+                (11, "lin".to_string()),
+                (15, "(".to_string()),
+                (16, "lin".to_string()),
+                (20, "bool".to_string()),
+                (25, "*".to_string()),
+                (27, "lin".to_string()),
+                (31, "bool".to_string()),
+                (35, ")".to_string()),
+                (37, "{".to_string()),
+                (38, "split".to_string()),
+                (44, "x".to_string()),
+                (46, "as".to_string()),
+                (49, "a".to_string()),
+                (50, ",".to_string()),
+                (52, "b".to_string()),
+                (54, "{".to_string()),
+                (55, "free".to_string()),
+                (60, "b".to_string()),
+                (61, ";".to_string()),
+                (63, "a".to_string()),
+                (64, "}".to_string()),
+                (65, "}".to_string())
+            ]
+        );
+    }
+}
