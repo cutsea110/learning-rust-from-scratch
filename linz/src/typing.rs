69365947572b87c95f65fc9a2af0e55ae3938880
cutsea110 <cutsea110@gmail.com>
2023-10-08 14:04:54 +0900

    inplement typing

--- linz/src/typing.rs
+++ linz/src/typing.rs
@@ -94,3 +94,235 @@ impl TypeEnv {
         }
     }
 }
+
+type TResult<'a> = Result<lang::TypeExpr, Cow<'a, str>>;
+
+/// 型付け関数
+/// 式を受け取り, 型を返す
+pub fn typing<'a>(expr: &lang::Expr, env: &mut TypeEnv, depth: usize) -> TResult<'a> {
+    match expr {
+        lang::Expr::App(e) => typing_app(e, env, depth),
+        lang::Expr::QVal(e) => typing_qval(e, env, depth),
+        lang::Expr::Free(e) => typing_free(e, env, depth),
+        lang::Expr::If(e) => typing_if(e, env, depth),
+        lang::Expr::Split(e) => typing_split(e, env, depth),
+        lang::Expr::Var(e) => typing_var(e, env, depth),
+        lang::Expr::Let(e) => typing_let(e, env, depth),
+    }
+}
+
+/// 関数適用の型付け
+fn typing_app<'a>(expr: &lang::AppExpr, env: &mut TypeEnv, depth: usize) -> TResult<'a> {
+    // 関数部分
+    let t1 = typing(&expr.expr1, env, depth)?;
+    let t_arg;
+    let t_ret;
+    match t1.prim {
+        lang::PrimType::Arrow(a, b) => {
+            t_arg = a; // 引数の型
+            t_ret = b; // 返り値の型
+        }
+        _ => return Err("関数型でない".into()),
+    }
+
+    // 引数部分
+    let t2 = typing(&expr.expr2, env, depth)?;
+
+    // 引数の型が一致しているかチェック
+    if *t_arg == t2 {
+        Ok(*t_ret)
+    } else {
+        Err("関数適用時における引数の型が異なる".into())
+    }
+}
+
+/// 修飾子付き値の型付け
+fn typing_qval<'a>(expr: &lang::QValExpr, env: &mut TypeEnv, depth: usize) -> TResult<'a> {
+    // プリミティブ型を計算
+    let p = match &expr.val {
+        lang::ValExpr::Bool(_) => lang::PrimType::Bool,
+        lang::ValExpr::Pair(e1, e2) => {
+            // 式 e1 と e2 を typing により型付け
+            let t1 = typing(e1, env, depth)?;
+            let t2 = typing(e2, env, depth)?;
+
+            // expr.qual が Un であり
+            // e1 か e2 の型に lin が含まれていた場合は型付けエラー
+            if expr.qual == lang::Qual::Un
+                && (t1.qual == lang::Qual::Lin || t2.qual == lang::Qual::Lin)
+            {
+                return Err("un型のペア内でlin型を利用している".into());
+            }
+
+            // ペア型を返す
+            lang::PrimType::Pair(Box::new(t1), Box::new(t2))
+        }
+        lang::ValExpr::Fun(e) => {
+            // 関数の型付け
+
+            // un 型の関数内では, lin 型の自由変数をキャプチャできないため
+            // lin 用の型環境を置き換え
+            let env_prev = if expr.qual == lang::Qual::Un {
+                Some(mem::take(&mut env.env_lin))
+            } else {
+                None
+            };
+
+            // depth をインクリメントして push
+            let mut depth = depth;
+            safe_add(&mut depth, &1, || "変数スコープのネストが深すぎる")?;
+            env.push(depth);
+            env.insert(e.var.clone(), e.ty.clone());
+
+            // 関数中の式を型付け
+            let t = typing(&e.expr, env, depth)?;
+
+            // スタックを pop し, pop した型環境の中に lin 型が含まれていた場合は型付けエラー
+            let (elin, _) = env.pop(depth);
+            for (k, v) in elin.unwrap().iter() {
+                if v.is_some() {
+                    return Err(format!(r#"関数定義内でlin型の変数"{k}"を消費していない"#).into());
+                }
+            }
+
+            // lin 用の型環境を復元
+            if let Some(ep) = env_prev {
+                env.env_lin = ep;
+            }
+
+            // 関数型を返す
+            lang::PrimType::Arrow(Box::new(e.ty.clone()), Box::new(t))
+        }
+    };
+
+    // 修飾子付き型を返す
+    Ok(lang::TypeExpr {
+        qual: expr.qual,
+        prim: p,
+    })
+}
+
+/// free 式の型付け
+fn typing_free<'a>(expr: &lang::FreeExpr, env: &mut TypeEnv, depth: usize) -> TResult<'a> {
+    if let Some((_, t)) = env.env_lin.get_mut(&expr.var) {
+        if t.is_some() {
+            *t = None;
+            return typing(&expr.expr, env, depth);
+        }
+    }
+    Err(format!(
+        r#"すでにfreeしたか、lin型ではない変数"{}"をfreeしている"#,
+        expr.var
+    )
+    .into())
+}
+
+/// if 式の型付け
+fn typing_if<'a>(expr: &lang::IfExpr, env: &mut TypeEnv, depth: usize) -> TResult<'a> {
+    let t1 = typing(&expr.cond_expr, env, depth)?;
+    //条件式の型は bool
+    if t1.prim != lang::PrimType::Bool {
+        return Err("ifの条件式がboolでない".into());
+    }
+
+    let mut e = env.clone();
+    let t2 = typing(&expr.then_expr, &mut e, depth)?;
+    let t3 = typing(&expr.else_expr, env, depth)?;
+
+    // then と else 式の型は同じで
+    // then と else 式の評価後の型環境が同じかチェック
+    if t2 != t3 || e != *env {
+        return Err("if式のthen節とelse節の式の型が異なる".into());
+    }
+
+    Ok(t2)
+}
+
+/// split 式の型付け
+fn typing_split<'a>(expr: &lang::SplitExpr, env: &mut TypeEnv, depth: usize) -> TResult<'a> {
+    if expr.left == expr.right {
+        return Err("splitの変数名が同じ".into());
+    }
+
+    let t1 = typing(&expr.expr, env, depth)?;
+    let mut depth = depth;
+    safe_add(&mut depth, &1, || "変数スコープのネストが深すぎる")?;
+
+    match t1.prim {
+        lang::PrimType::Pair(p1, p2) => {
+            env.push(depth);
+            // ローカル変数の型を追加
+            env.insert(expr.left.clone(), *p1);
+            env.insert(expr.right.clone(), *p2);
+        }
+        _ => {
+            return Err("splitの引数がペア型でない".into());
+        }
+    }
+
+    let ret = typing(&expr.body, env, depth);
+
+    // ローカル変数を削除
+    let (elin, _) = env.pop(depth);
+
+    // lin 型の変数を消費しているかチェック
+    for (k, v) in elin.unwrap().iter() {
+        if v.is_some() {
+            return Err(format!(r#"splitの式内でlin型の変数"{k}"を消費していない"#).into());
+        }
+    }
+
+    ret
+}
+
+/// 変数の型付け
+fn typing_var<'a>(expr: &str, env: &mut TypeEnv, depth: usize) -> TResult<'a> {
+    let ret = env.get_mut(expr);
+    if let Some(it) = ret {
+        // 定義されている
+        if let Some(t) = it {
+            // 消費されていない
+            if t.qual == lang::Qual::Lin {
+                // lin 型
+                let eret = t.clone();
+                *it = None; // lin を消費
+                return Ok(eret);
+            } else {
+                return Ok(t.clone());
+            }
+        }
+    }
+
+    Err(format!(
+        r#""{}"という変数は定義されていないか、利用済みか、キャプチャできない"#,
+        expr
+    )
+    .into())
+}
+
+/// let 式の型付け
+fn typing_let<'a>(expr: &lang::LetExpr, env: &mut TypeEnv, depth: usize) -> TResult<'a> {
+    // 変数束縛
+    let t1 = typing(&expr.expr1, env, depth)?;
+    // 束縛変数の型をチェック
+    if t1 != expr.ty {
+        return Err(format!(r#"変数"{}"の型が異なる"#, expr.var).into());
+    }
+
+    // 関数内
+    let mut depth = depth;
+    safe_add(&mut depth, &1, || "変数スコープのネストが深すぎる")?;
+    env.push(depth);
+    env.insert(expr.var.clone(), t1); // 変数の型を insert
+    let t2 = typing(&expr.expr2, env, depth)?;
+
+    // lin 型の変数を消費しているかチェック
+    let (elin, _) = env.pop(depth);
+    for (k, v) in elin.unwrap().iter() {
+        if v.is_some() {
+            return Err(format!(r#"let式内でlin型の変数"{k}"を消費していない"#).into());
+        }
+    }
+
+    Ok(t2)
+}
